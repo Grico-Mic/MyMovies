56f62474a399a9693fb35fb22e67d330fc25509f
Grico-Mic <igorgric@outlook.com>
2023-04-28 23:57:03 +0200

    Created MVC template app.

---
+++ MyMovies/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace MyMovies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Ganre { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
