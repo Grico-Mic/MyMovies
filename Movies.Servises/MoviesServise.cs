98643fd037dc4ee23238ea8d7c5eea672318d099
Grico-Mic <igorgric@outlook.com>
2023-05-06 22:09:48 +0200

    Implemented n-tire architecture

---
+++ Movies.Servises/MoviesServise.cs
@@ -0,0 +1,29 @@
+﻿using MyMovies.Models;
+using MyMovies.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Movies.Servises
+{
+    public class MoviesServise
+    {
+        private MoviesRepository _moviesRepository { get; set; }
+        public MoviesServise()
+        {
+            _moviesRepository = new MoviesRepository();
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return _moviesRepository.GetAll();
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            return _moviesRepository.GetById(id);
+        }
+    }
+       
+}
+  
