7d4df00378416a5f26cc04231916216fb000e08a
Grico-Mic <igorgric@outlook.com>
2023-05-23 20:31:20 +0200

    Impelemented sql repository.

--- MyMovies/Startup.cs
+++ MyMovies/Startup.cs
@@ -24,7 +24,7 @@ namespace MyMovies
         {
             services.AddControllersWithViews();
             services.AddTransient<IMoviesServise,MoviesServise>();
-            services.AddTransient<IMoviesRepository, MoviesFileRepository>();
+            services.AddTransient<IMoviesRepository, MoviesSqlRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
