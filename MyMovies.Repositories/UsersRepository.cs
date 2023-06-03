c6711e97d580ad40452b4e687f3892e7971f93a2
Grico-Mic <igorgric@outlook.com>
2023-06-03 17:00:24 +0200

    Create repository for model User.

---
+++ MyMovies.Repositories/UsersRepository.cs
@@ -0,0 +1,22 @@
+﻿using MyMovies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyMovies.Repositories
+{
+    public class UsersRepository
+    {
+        private MyMoviesDbContext _context;
+
+        public UsersRepository(MyMoviesDbContext context)
+        {
+            this._context = context;
+        }
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+    }
+}
