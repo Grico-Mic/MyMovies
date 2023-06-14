75881a49524eaf094bc9b7c39d708652119b54c9
Grico-Mic <igorgric@outlook.com>
2023-06-14 17:30:12 +0200

    Added razor runtime compilation.

--- MyMovies/Startup.cs
+++ MyMovies/Startup.cs
@@ -27,6 +27,8 @@ namespace MyMovies
             services.AddControllersWithViews();
             services.AddTransient<IMoviesServise,MoviesServise>();
             services.AddTransient<IMoviesRepository, MoviesRepository>();
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
